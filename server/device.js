b96988edf0d8bbc5acaa07a6097bc3ec4dc5fcfe
Sharad Vikram <sharad.vikram@gmail.com>
2013-01-18 19:17:13 -0800

    added sample

--- server/device.js
+++ server/device.js
@@ -1,30 +1,73 @@
 var TAP = "tap";
-var net = require('socket');
+var net = require('net');
 var Device = function(name, driver) {
   this.name = name;
-  this.tap = function() {
-    driver.action(name, 'tap', {});
+  this.tap = function(callback) {
+    driver.action(name, 'tap', {}, callback);
   }
-  this.action = function(action, options) {
+  this.action = function(action, options, callback) {
     if (action == TAP) {
       this.tap();
     } else {
-      driver.action(name, action, options);
+      driver.action(name, action, options, callback);
     }
   }
 }
+var RemoteClient = function(drivers) {
+    var server = net.createServer(function(socket){
+      socket.on('data', function(data) {
+        var obj = JSON.parse(data);
+        if (!drivers[obj.name] || !drivers[obj.name][obj.action]) {
+          socket.write(JSON.stringify({
+              status : "failure"
+          }));
+          return;
+        }
+        drivers[obj.name][obj.action](obj.options, function() {
+          socket.write(JSON.stringify({
+              status : "success"
+          }));
+        });
+      })
+    });
+    this.listen = function(port) {
+      server.listen(port);
+      console.log("Remote listening on %s", port);
+    }
+}
 var Remote = function(ip, port) {
-  this.action = function(name, action, options) {
+  this.action = function(name, action, options, callback) {
+    var client = new net.Socket();
+    var obj = {
+      name : name,
+      action : action,
+      options : options
+    }
+    client.connect(port, ip, function() {
+      var write = JSON.stringify(obj);
+      console.log(write);
+      client.write(write);
+    })
+    client.on('data', function(data) {
+      var result = JSON.parse(data);
+      client.destroy();
+      if (callback)
+        callback(result);
+    })
   }
 }
-var Local = function(options) {
-  this.action = function(name, action, options) {
+var LocalClient = function(drivers) {
+  this.action = function(name, action, options, callback) {
+    drivers[name][action](options, callback);
   }
 }
-module.exports = {;
+module.exports = {
   Driver : {
       Remote : Remote,
-      Local : Local
   },
-  Device : Device
+  Device : Device,
+  Client : {
+    Remote : RemoteClient,
+    Local : LocalClient
+  }
 }
---
+++ server/sample.js
@@ -0,0 +1,23 @@
+var Devices = require("./device")
+var devices = {
+    peter : {
+        tap : function(options, callback) {
+            console.log("I WAS TAPPED");
+            callback({ status : 'success'})
+        },
+        on : function(options, callback) {
+            console.log(arguments);
+            callback();
+        }
+    }
+}
+//SET UP
+var remote = new Devices.Client.Remote(devices);
+remote.listen(8080);
+
+var client = new Devices.Driver.Remote('localhost', 8080);
+var peter = new Devices.Device("peter", client);
+console.log(peter)
+peter.tap(function(result) {
+    console.log(result);
+});
