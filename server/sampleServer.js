c6f518443b441e53abc2fcc5aadad9be3b70ceba
Sharad Vikram <sharad.vikram@gmail.com>
2013-01-18 19:30:27 -0800

    swapped

--- server/sampleServer.js
+++ server/sampleServer.js
@@ -1,6 +1,16 @@
-var Devices = require('../lib/device')
-var client = new Devices.Driver.Remote('localhost', 8080);
-var peter = new Devices.Device("peter", client);
-peter.tap(function(result) {
-    console.log(result);
-});
+var Devices = require("../lib/device")
+var devices = {
+    peter : {
+        tap : function(options, callback) {
+            console.log("I WAS TAPPED");
+            callback({ status : 'success'})
+        },
+        on : function(options, callback) {
+            console.log(arguments);
+            callback({ status : "success"});
+        }
+    }
+}
+//SET UP
+var remote = new Devices.Client.Remote(devices);
+remote.listen(8080);
