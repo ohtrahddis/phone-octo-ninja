38360a5587c6bf701e3ba273e3a48269d3fd15ef
Sharad Vikram <sharad.vikram@gmail.com>
2013-01-18 21:29:07 -0800

    fixed binaryremote

--- dist/binaryremote.js
+++ dist/binaryremote.js
@@ -3,13 +3,13 @@ var BinaryRemote = function(ip, port, drivers) {
   var client = new BinaryClient("ws://"+ip+":"+port);
   client.on('stream', function(stream)  {
     stream.on('data', function(data) {
-      if (!drivers[obj.name] || !drivers[obj.name][obj.action]) {
+      if (!drivers[obj.action]) {
         stream.write(JSON.stringify({
             status : "failure"
         }));
         return;
       }
-      drivers[obj.name][obj.action](obj.options, function(results) {
+      drivers[obj.action](obj.options, function(results) {
         socket.write(JSON.stringify(results));
       });
     });
